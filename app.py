825ebeca52e720a72e2a5edea9aebe9958315e76
mohamadanasfattoum <samaanas30@gmail.com>
2025-05-15 19:16:18 +0200

    Refactor app.py to include ChatOllama initialization and response invocation

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 from langchain_ollama.chat_models import ChatOllama
+
+
+
+
+llm = ChatOllama(
+    model = "llama3.2:3b",
+    temperature = 0.8,
+    num_predict = 100,
+)
+
+response = llm.invoke("tell me more about python?")
+print(response)

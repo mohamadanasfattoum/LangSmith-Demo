f653be17d32510591ce2b41e04563df412a4c12d
mohamadanasfattoum <samaanas30@gmail.com>
2025-05-15 19:45:07 +0200

    Add dotenv loading and create .gitignore for project dependencies. and created langSmith project

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from langchain_ollama.chat_models import ChatOllama
+from dotenv import load_dotenv
+
+# Load environment variables from a .env file
+load_dotenv()
 
 
-
-
+# Initialize the ChatOllama model with the specified parameters
 llm = ChatOllama(
     model = "llama3.2:3b",
     temperature = 0.8,
